44cc3ab8f22cddca71b399ed312665ba1e863d2f
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 23:07:23 +0700

    js lab 143

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+// จงสร้าง object โดยมี key เป็น id ของ product และ value เป็น product name
+
+const products = [
+  { id: 1, name: 'Crystal' },
+  { id: 4, name: 'Namthip' },
+  { id: 5, name: 'Nestle' }
+];
+
+const productObject = products.reduce((obj, product) => {
+  obj[product.id] = { name: product.name };
+  return obj;
+}, {});
+
+console.log(productObject);
+// { 1: { name: 'Crystal' }, 4: { name: 'Namthip' }, 5: { name: 'Nestle' } };
